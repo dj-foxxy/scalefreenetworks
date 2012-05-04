d116218625f761a8fdef06bdd5e514a9ddb997c9
Peter Robert Sutton <petersutton2009@gmail.com>
2012-05-04 13:59:13 +0100

    Added command line handling and get library code.

--- Makefile
+++ Makefile
@@ -1,12 +1,21 @@
 all: build
 
-build:
+build: lib
 	make -C src/
 
 clean:
 	rm -fr bin/
 
+deep-clean: clean
+	rm -fr lib/
+
+lib:
+	mkdir -p lib/
+	wget -O - http://prdownloads.sourceforge.net/argtable/argtable2-13.tar.gz \
+		| tar --directory=lib/ -xz
+	(cd lib/argtable2-13 && ./configure && make)
+
 rebuild: clean build
 
-.PONY: all build clean
+.PONY: all build clean deep-clean
 
--- src/sfn.c
+++ src/sfn.c
@@ -1,7 +1,37 @@
 #include<stdlib.h>
 
-int main(int const argc, char const *const *const argv)
+#include <argtable2.h>
+
+#define SFN_PROG_NAEM "sfn"
+
+int main(int const argc, char **argv)
 {
-    return EXIT_SUCCESS;
+	struct arg_int *init_num_nodes = arg_int1("n", NULL, "<n>",
+			"Initial number of nodes.");
+	struct arg_end *end = arg_end(1);
+	void *arg_table[] = { init_num_nodes, end };
+
+	int exit_code = EXIT_SUCCESS;
+
+    if (arg_nullcheck(arg_table) != 0)
+	{
+		printf("[ERROR] Insufficient memory.\n");
+		exit_code = EXIT_FAILURE;
+		goto exit;
+	}
+
+	int const num_errors = arg_parse(argc, argv, arg_table);
+
+	if (num_errors > 0)
+	{
+		arg_print_errors(stdout, end, SFN_PROG_NAEM);
+		exit_code = EXIT_SUCCESS;
+		goto exit;
+	}
+
+exit:
+	arg_freetable(arg_table, sizeof(arg_table) / sizeof(arg_table[0]));
+
+	return exit_code;
 }
 
